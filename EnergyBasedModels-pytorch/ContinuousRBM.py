Removed attempt to use optimizers, standard Contrastive Divergence added. cuda_state changed by use_gpu Shuffling of the dataset fixed Added look-ahead stopping instead of fixed number of epochs

EnergyBasedModels-pytorch/ContinuousRBM.py
@@ -1,4 +1,4 @@
-# Restricted Boltzmann Machine with continuous-valued outputs, trained by Stochastic Gradient Descent
+# Restricted Boltzmann Machine with continuous-valued outputs
 #
 # Author: Alejandro Pozas-Kerstjens
 # Requires: numpy for numerics
@@ -6,7 +6,7 @@
 #           matplotlib for plots
 #           tqdm for progress bar
 #           imageio for output export
-# Last modified: Jan, 2018
+# Last modified: Feb, 2018
 
 import imageio
 import matplotlib.pyplot as plt
@@ -14,90 +14,134 @@ import numpy as np
 import os
 import torch
 import torch.nn as nn
-import torch.optim as optim
+import torch.nn.functional as F
 from RBM import RBM
 from torch.autograd import Variable
 from torchvision import datasets
 from tqdm import tqdm
 
 class CRBM(RBM):
-    def sample_v_given_h(self, h0_sample):
-    # According to Hinton, this (using the activation probabilities as outputs themselves) should be
-    # enough to get continuous outputs
-        a_h = self.propdown(h0_sample)
-        return [None, a_h]
-
+    def sample_v_given_h(self, h):
+        '''This method of sampling is described in
+        
+        Bengio, Y., Lamblin, P., Popovici, D., and Larochelle, H.,
+        Greedy Layer-wise Training of Deep Networks
+        
+        But it seems not to work better than keeping the probabilities of
+        activation as the outputs themselves
+        '''
+        # a       = F.linear(h, self.W.t(), self.vbias)
+        # a       += 1e-7    # For numerical stability
+        # v_probs = F.sigmoid(a)
+        # U       = Variable(torch.rand(a.size()))
+        # zr      = Variable(torch.zeros(a.size()))
+        # if a.is_cuda:
+            # U  = U.cuda()
+            # zr = zr.cuda()
+        # mask     = torch.max(zr, a)
+        # v_sample = torch.div(mask
+        #                      + (U * (a - mask).exp()
+        #                         + (1 - U) * mask.neg().exp()).log(),
+        #                      a)
+        # return [v_probs, v_sample]
+        v_probs = self.propdown(h)
+        return [None, v_probs]
+        
+def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2, 
+             k_reconstruct=100, batch_size=30, model_dir='CRBM.h5', 
+             best_dir='CRBM_best.h5', use_gpu=True):
     
-def test_rbm(hidd=200, learning_rates=[1e-3] * 10, k=2, batch_size=30, cuda_state=True):
-    
-    data = datasets.MNIST('mnist', train=True, download=True).train_data.type(torch.FloatTensor)
-    test = datasets.MNIST('mnist', train=False).test_data.type(torch.FloatTensor)
+    data = datasets.MNIST('mnist',
+                          train=True,
+                          download=True).train_data.type(torch.FloatTensor)
+    test = datasets.MNIST('mnist',
+                          train=False).test_data.type(torch.FloatTensor)
     
     data = data.view((-1, 784)) / 255
     test = test.view((-1, 784)) / 255
+    
+    vis  = len(data[0])
+    
+    # According to Hinton this initialization of the visible biases should be
+    # fine, but some biases diverge in the case of MNIST.
+    # Actually, this initialization is the inverse of the sigmoid. This is, it
+    # is the inverse of p = sigm(vbias), so it can be expected that during
+    # training the weights are close to zero and change little
+    vbias = nn.Parameter(torch.log(
+                                   data.mean(0) / (1 - data.mean(0))
+                                   ).clamp(-20, 20))
 	
-    vis  = len(data[0])                    
-    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True)
-
-    # According to Hinton this should be fine, but some biases diverge in the case of MNIST.
-    # Actually, this initialization is the inverse of the sigmoid. This is, it is the inverse of 
-    # p = sigm(vbias), so it can be expected that during training the weights are close to zero and
-    # change little
-    vbias = nn.Parameter(torch.log(data.mean(0) / (1 - data.mean(0))).clamp(-20, 20))
-	
-    # ------------------------------------------------------------------------------
-    # Construct CRBM
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # Construct RBM
+    # -------------------------------------------------------------------------
     pre_trained = os.path.isfile('CRBM.h5')
-    rbm         = CRBM(n_visible=vis, n_hidden=hidd, k=k, cuda_state=cuda_state, vbias=vbias)
+    rbm         = CRBM(n_visible=vis,
+                       n_hidden=hidd,
+                       k=k,
+                       use_gpu=use_gpu,
+                       vbias=vbias)
     if pre_trained:
         rbm.load_state_dict(torch.load('CRBM.h5'))
     
-    if cuda_state:
-        rbm=rbm.cuda()
-    
-    # ------------------------------------------------------------------------------
+    if use_gpu:
+        rbm = rbm.cuda()
+    # -------------------------------------------------------------------------
     # Training
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     if not pre_trained:
         validation = Variable(data)[:10000]
         test       = Variable(test)
-        if cuda_state:
-            validation = validation.cuda()
-            test       = test.cuda()
-        for epoch, learning_rate in enumerate(learning_rates):
-            train_op   = optim.SGD(rbm.parameters(), lr=learning_rate)
-            rbm.train(train_loader, train_op, epoch)
-	    # A good measure of well-fitting is the free energy difference between some known and
-	    # unknown instances. It is related to the log-likelihood difference, but it does not
-	    # depend on the partition function. It should be around 0, and if it grows, it might be
-	    # overfitting to the training data
-            gap = rbm.free_energy(validation) - rbm.free_energy(test)
-            print('Gap = ' + str(gap.data[0]))
-        torch.save(rbm.state_dict(), 'CRBM.h5')
+        best_gap   = np.inf
+        look_ahead = 0
+        epoch = 1
+        while True:
+            train_loader = torch.utils.data.DataLoader(data,
+                                                       batch_size=batch_size,
+                                                       shuffle=True)
+            metrics      = rbm.train(train_loader, learning_rate, epoch)
+            # A good measure of well-fitting is the free energy difference
+            # between some known and unknown instances. It is related to the
+            # log-likelihood difference, but it does not depend on the
+            # partition function. It should be around 0, and if it grows, it
+            # might be overfitting to the training data.
+            # High-probability instances have very negative free energy, so the
+            # gap becoming very negative is sign of overfitting.
+            gap = (rbm.free_energy(validation) - rbm.free_energy(test)).mean(0)
+            print('Rec. error = ' + str(metrics))
+            print('Gap = {}'.format(gap.data[0]))
+            if abs(gap.data[0]) < best_gap:
+                best_gap = abs(gap.data[0])
+                print('Gap is smaller than best previous, saving weights...')
+                torch.save(rbm.state_dict(), best_dir)
+                look_ahead = 0
+            look_ahead += 1
+            if look_ahead == max_look_ahead:
+                print('It has been {} epochs since the last improvement. '
+                      .format(str(max_look_ahead)) + 'Stopping training...')
+                break
+            epoch += 1
+        torch.save(rbm.state_dict(), model_dir)
     
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Plotting
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     print('Reconstructing images')
     plt.figure(figsize=(20, 10))
     zero = Variable(torch.zeros(25, 784))
-    if cuda_state:
-        zero = zero.cuda()
-    images = [zero.data.cpu().numpy().reshape((5 * 28, 5 * 28))]
-    for i in range(20000):
+    images = [zero.data.numpy().reshape((5 * 28, 5 * 28))]
+    for i in range(k_reconstruct):
         _, zero = rbm.sample_h_given_v(zero)
         _, zero = rbm.sample_v_given_h(zero)
-        if i % 32 == 0:
+        if i % 3 == 0:
             datas = zero.data.cpu().numpy().reshape((25, 28, 28))
             image = np.zeros((5 * 28, 5 * 28))
             for k in range(5):
                 for l in range(5):
                     image[28*k:28*(k+1), 28*l:28*(l+1)] = datas[k + 5*l, :, :]
             images.append(image)
-    imageio.mimsave('crbm_sample.gif', images, duration=0.1)
-
+    imageio.mimsave('CRBM_sample.gif', images, duration=0.1)
 	
 if __name__ == "__main__":
-    learning_rates = [1e-2] * 10
-    test_rbm(hidd=30, learning_rates=learning_rates, k=2, batch_size=20, cuda_state=True)
+    test_rbm(hidd=30, learning_rate=1e-3, max_look_ahead=20, k=20,
+             k_reconstruct=2000, batch_size=20, model_dir='CRBM.h5',
+             best_dir='CRBM_best.h5', use_gpu=True)

EnergyBasedModels-pytorch/DBN.py
@@ -1,4 +1,4 @@
-# Deep Belief Network
+# Deep Belief Network pre-trained greedily via Contrastive Divergence
 #
 # Author: Alejandro Pozas-Kerstjens
 # Requires: numpy for numerics
@@ -16,29 +16,16 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 from copy import deepcopy
-from RBM import RBM
+from RBM import RBM, outer_product
 from torch.autograd import Variable
 from torchvision import datasets
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-def outer_product(vecs1, vecs2):
-    '''Computes the outer product of batches of vectors
-       :param vecs1: b 1-D tensors of length m
-       :type vecs1: list of torch.Tensor or torch.autograd.Variable
-       :param vecs2: b 1-D tensors of length n
-       :type vecs2: list of torch.Tensor or torch.autograd.Variable
-       
-       :returns: torch.Tensor or torch.autograd.Variable of size (m, n)
-       '''
-    return torch.bmm(vecs1.unsqueeze(2), vecs2.unsqueeze(1))
-
-
 class DBN(object):
-    def __init__(self, n_visible=6, hidden_layer_sizes=[3, 3], \
+    def __init__(self, n_visible=6, hidden_layer_sizes=[3, 3],
                  k=5, use_gpu=True):
         
         self.k       = k
@@ -70,14 +57,15 @@ class DBN(object):
             
 
 
-    def pretrain(self, input, lr=0.1, epochs=100, batch_size=10, test=None):
+    def pretrain(self, input_data, lr=0.1, max_look_ahead=15,
+                 batch_size=10, test=None):
         # Pre-train the DBN as individual RBMs
         for i in range(self.n_layers):
             print('#########################################')
             print('#        Pre-training layers {}-{}        #'.format(i, i+1))
             print('#########################################')
             if i == 0:
-                layer_input = input
+                layer_input = input_data
             else:
                 layer_input = Variable(layer_input)
                 if self.use_gpu:
@@ -88,27 +76,40 @@ class DBN(object):
                 if test is not None:
                     _, test = self.rbm_layers[i-1].sample_h_given_v(test)
             
-            rbm          = self.rbm_layers[i]
-            optimizer    = optim.SGD(rbm.parameters(), lr=lr)
-            layer_loader = torch.utils.data.DataLoader(layer_input,
-                                                       batch_size=batch_size,
-                                                       shuffle=True)
-            for epoch in range(epochs):
-                metrics = rbm.train(input=layer_loader,
-                                    optimizer=optimizer,
-                                    epoch=epoch)
-                print('Loss: ' + str(metrics[0]))
-                print('Reconstruction accuracy: ' + str(metrics[1]))
+            rbm = self.rbm_layers[i]
+            best_gap = np.inf
+            epoch = 1
+            look_ahead = 0
+            while True:
+                layer_loader = torch.utils.data.DataLoader(layer_input,
+                                                         batch_size=batch_size,
+                                                         shuffle=True)
+                rbm.train(input_data=layer_loader, lr=lr, epoch=epoch)
                 if test is not None:
                     validation = Variable(layer_input)[:10000]
                     if self.use_gpu:
                         validation = validation.cuda()
                         test   = test.cuda()
-                    gap = rbm.free_energy(validation) - rbm.free_energy(test)
+                    val_fe  = rbm.free_energy(validation).mean(0)
+                    test_fe = rbm.free_energy(test).mean(0)
+                    gap = val_fe - test_fe
                     print('Gap: ' + str(gap.data[0]))
-                
+                    if abs(gap.data[0]) < best_gap:
+                        best_gap = abs(gap.data[0])
+                        print('Gap is smaller than best previous, ' +
+                              'saving weights...')
+                        best_weights = rbm.state_dict()
+                        look_ahead = 0
+                look_ahead += 1
+                if look_ahead == max_look_ahead:
+                    print('It has been {} epochs since the last improvement. '
+                         .format(str(max_look_ahead)) + 'Stopping training...')
+                    break
+                epoch += 1
+            rbm.load_state_dict(best_weights)
 
-    def finetune(self, input, lr=0.1, epochs=100, batch_size=10, k=None):
+
+    def finetune(self, input_data, lr=0.1, epochs=100, batch_size=10, k=None):
         '''This is done according to the up-down method developed in
            Hinton et al., A fast learning algorithm for deep belief nets.
            Neural Computation 18, 1527-1554 (2006)
@@ -118,7 +119,7 @@ class DBN(object):
         print('#########################################')
         if k is not None:
             self.k = k
-        input_loader = torch.utils.data.DataLoader(input,
+        input_loader = torch.utils.data.DataLoader(input_data,
                                                    batch_size=batch_size,
                                                    shuffle=True)
         top_RBM      = self.rbm_layers[-1]
@@ -144,15 +145,15 @@ class DBN(object):
                 wakepos_samples.append(sample)
                 # We use these parameters to get the positive phase of the top
                 # layer, which is trained as a standard RBM
-                pos_W_topRBM = outer_product(wakepos_samples[-1],
-                                             wakepos_samples[-2])
+                pos_W_topRBM = outer_product(wakepos_samples[-2],
+                                             wakepos_samples[-1])
                 # Perform Gibbs sampling iterations in the top RBM
                 h_sample = sample    # To initialize loop
                 for _ in range(self.k):
                     _, v_sample = top_RBM.sample_v_given_h(h_sample)
                     _, h_sample = top_RBM.sample_h_given_v(v_sample)
                 # Take negative phase statistics on the top RBM for CD
-                neg_W_topRBM = outer_product(h_sample, v_sample)
+                neg_W_topRBM = outer_product(v_sample, h_sample)
 
                 # Beginning at the end of the Gibbs sampling, perform a
                 # top-down generative pass to obtain the sleep positive phase
@@ -240,13 +241,13 @@ class DBN(object):
             layer.load_state_dict(dicts[i])
 
 
-def test_dbn(pretrain_lr=1e-2, pretraining_epochs=30, k=5, finetune_lr=1e-3, \
+def test_dbn(pretrain_lr=1e-2, look_ahead_pretrain=20, k=5, finetune_lr=1e-3,
              finetune_epochs=30, batch_size=20, use_gpu=True):
 
-    data = datasets.MNIST('../mnist',
+    data = datasets.MNIST('mnist',
                           train=True,
                           download=True).train_data.type(torch.FloatTensor)
-    test = datasets.MNIST('../mnist',
+    test = datasets.MNIST('mnist',
                           train=False).test_data.type(torch.FloatTensor)
     
     data = data.view((-1, 784)) / 255
@@ -272,17 +273,15 @@ def test_dbn(pretrain_lr=1e-2, pretraining_epochs=30, k=5, finetune_lr=1e-3, \
     # Training
     # -------------------------------------------------------------------------
     if not pre_trained:
-        validation = Variable(data)[:10000]
         test       = Variable(test)
         if use_gpu:
-            validation = validation.cuda()
             test       = test.cuda()
-        dbn.pretrain(input=data,
+        dbn.pretrain(input_data=data,
                      lr=pretrain_lr,
-                     epochs=pretraining_epochs,
+                     max_look_ahead=look_ahead_pretrain,
                      batch_size=batch_size,
                      test=test)
-        dbn.finetune(input=data,
+        dbn.finetune(input_data=data,
                      lr=finetune_lr,
                      epochs=finetune_epochs,
                      batch_size=batch_size)
@@ -314,10 +313,8 @@ def test_dbn(pretrain_lr=1e-2, pretraining_epochs=30, k=5, finetune_lr=1e-3, \
             for l in range(5):
                 image[28*k:28*(k+1), 28*l:28*(l+1)] = datas[k + 5*l, :, :]
         images.append(image)
-    imageio.mimsave('dbn_sample.gif', images, duration=0.5)
-    for i in range(10):
-        plt.imsave('dbn_sample_{}.png'.format(i), images[20*i])
+    imageio.mimsave('DBN_sample.gif', images, duration=0.1)
                                    
 if __name__ == "__main__":
-    test_dbn(pretrain_lr=1e-2, pretraining_epochs=15, k=2, finetune_lr=1e-2, \
-             finetune_epochs=15, batch_size=20, use_gpu=True)
+    test_dbn(pretrain_lr=1e-2, look_ahead_pretrain=20, k=2, finetune_lr=1e-4,
+             finetune_epochs=10, batch_size=20, use_gpu=True)

EnergyBasedModels-pytorch/RBM.py
@@ -1,4 +1,4 @@
-# Restricted Boltzmann Machine trained by Stochastic Gradient Descent
+# Restricted Boltzmann Machine trained by Contrastive Divergence
 #
 # Author: Alejandro Pozas-Kerstjens
 # Requires: numpy for numerics
@@ -6,7 +6,7 @@
 #           matplotlib for plots
 #           tqdm for progress bar
 #           imageio for output export
-# Last modified: Jan, 2018
+# Last modified: Feb, 2018
 
 import imageio
 import matplotlib.pyplot as plt
@@ -15,122 +15,149 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from tqdm import tqdm
 
 
+def outer_product(vecs1, vecs2):
+    '''Computes the outer product of batches of vectors
+       :param vecs1: b 1-D tensors of length m
+       :type vecs1: list of torch.Tensor or torch.autograd.Variable
+       :param vecs2: b 1-D tensors of length n
+       :type vecs2: list of torch.Tensor or torch.autograd.Variable
+       
+       :returns: torch.Tensor or torch.autograd.Variable of size (m, n)
+       '''
+    return torch.bmm(vecs1.unsqueeze(2), vecs2.unsqueeze(1))
+    
+    
+def log1pexp(tensor):
+    '''Computes pointwise log(1+exp()) for all elements in a torch tensor. The
+       way of computing it without under- or overflows is through the
+       log-sum-exp trick, namely computing
+       
+       log(1+exp(x)) = a + log(exp(-a) + exp(x-a))     with a = max(0, x)
+       
+       The function is adapted to be used in GPU if needed.
+       :param tensor: torch.Tensor or torch.autograd.Variable of arbitrary size
+       
+       :returns: torch.Tensor or torch.autograd.Variable
+       '''
+    zr = Variable(torch.zeros(tensor.size()))
+    if tensor.is_cuda:
+        zr = zr.cuda()
+    a = torch.max(zr, tensor)
+    return a + (a.neg().exp() + (tensor - a).exp()).log()
+
+
 class RBM(nn.Module):
-    def __init__(self, input=None, n_visible=100, n_hidden=50, k=5,
-	             cuda_state=True, W=None, hbias=None, vbias=None):
+    def __init__(self, n_visible=100, n_hidden=50, k=5,
+	             use_gpu=True, W=None, hbias=None, vbias=None):
         
         super(RBM, self).__init__()
-        self.k           = k
-        self.input       = input
-        self.cuda_state  = cuda_state
-
+        self.k        = k
+        self.use_gpu  = use_gpu
         if W is not None:
             self.W = W
         else:
-            self.W = nn.Parameter(0.01 * torch.randn(n_hidden, n_visible))  # initialize W normally
+            self.W = nn.Parameter(0.01 * torch.randn(n_hidden, n_visible))
         
         if hbias is not None:
             self.hbias = hbias
         else:
-            self.hbias = nn.Parameter(torch.zeros(n_hidden))  # initialize h bias to 0
+            self.hbias = nn.Parameter(torch.zeros(n_hidden))
         
         if vbias is not None:
             self.vbias = vbias
         else:
-            self.vbias = nn.Parameter(torch.zeros(n_visible))  # initialize v bias to 0
+            self.vbias = nn.Parameter(torch.zeros(n_visible))
+
+
+    def forward(self, v0):
+        h0_probs, h0 = self.sample_h_given_v(v0)
+        _, v         = self.sample_v_given_h(h0)
+        for _ in range(self.k - 1):
+            h_probs, h = self.sample_h_given_v(v)
+            _ , v      = self.sample_v_given_h(h)
+        return v0, h0_probs, v, h_probs
         
-
-    def forward(self, v):
-        v0 = v
-        for _ in range(self.k):
-            _, h = self.sample_h_given_v(v)
-            _, v = self.sample_v_given_h(h)
-        return v0, v
-
-
+    
     def free_energy(self, v):
-        vbias_term = v.mv(self.vbias)
-        wx_b       = F.linear(v, self.W, self.hbias)
-
-        # Now we should do log(exp(wx_b) + 1). Instead, for numerical stability, we do the
-	# log-sum-exp trick, namely for large values of wx_b we do a + log(exp(wx_b - a) + exp(-a)),
-	# where a = max(0, wx_b).
-        zr = Variable(torch.zeros(wx_b.size()))
-        if self.cuda_state:
-            zr = zr.cuda()
-        mask        = torch.max(zr, wx_b)
-        hidden_term = ((wx_b - mask).exp().add(mask.neg().exp())).log().add(mask).sum(1)
-        return (-hidden_term - vbias_term).mean()
-
-
+        if (self.use_gpu and not v.is_cuda):
+            v = v.cuda()
+        vbias_term  = v.mv(self.vbias)
+        wx_b        = F.linear(v, self.W, self.hbias)
+        hidden_term = log1pexp(wx_b).sum(1)
+        return (-hidden_term - vbias_term)
+        
+        
     def propdown(self, h):
+        if (self.use_gpu and not h.is_cuda):
+            h = h.cuda()
         pre_sigmoid_activation = F.linear(h, self.W.t(), self.vbias)
         return F.sigmoid(pre_sigmoid_activation)
 
 
     def propup(self, v):
+        if (self.use_gpu and not v.is_cuda):
+            v = v.cuda()
         pre_sigmoid_activation = F.linear(v, self.W, self.hbias)
         return F.sigmoid(pre_sigmoid_activation)
 
 
     def reconstruct(self, v, k=None):
-        if self.cuda_state:
-            v = v.cuda()
         if k == None:
             k = self.k
         for _ in range(k):
             _, h = self.sample_h_given_v(v)
             _, v = self.sample_v_given_h(h)
         return v
-
-
+        
+        
     def sample_h_given_v(self, v):
-        h_mean   = self.propup(v)
-        h_sample = torch.bernoulli(h_mean).float()
-        if self.cuda_state:
-            h_sample = h_sample.cuda()
-        return [h_mean, h_sample]
+        h_probs  = self.propup(v)
+        h_sample = h_probs.bernoulli()
+        return [h_probs, h_sample]
 
 
     def sample_v_given_h(self, h):
-        v_mean   = self.propdown(h)
-        v_sample = torch.bernoulli(v_mean).float()
-        if self.cuda_state:
-            v_sample = v_sample.cuda()
-        return [v_mean, v_sample]
+        v_probs  = self.propdown(h)
+        v_sample = v_probs.bernoulli()
+        return [v_probs, v_sample]
 
 
-    def train(self, input, optimizer, epoch):
-        loss_ = []
+    def train(self, input_data, lr, epoch):
         error_ = []
-        count  = 0
-        for iter, batch in enumerate(tqdm(input, desc='Epoch ' + str(epoch + 1))):
+        for _, batch in enumerate(tqdm(input_data,
+                                       desc='Epoch ' + str(epoch))):
             sample_data = Variable(batch).float()
-            if self.cuda_state:
+            if self.use_gpu:
                 sample_data = sample_data.cuda()
-            v, v1 = self.forward(sample_data)
-            rec_error = F.binary_cross_entropy(v1, v)
+            v0, h0_probs, v1, h1_probs = self.forward(sample_data)
+            rec_error = F.binary_cross_entropy(v1, v0)
             error_.append(rec_error.data[0])
-            loss = self.free_energy(v) - self.free_energy(v1.detach())
-            loss_.append(loss.data[0])
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print('Loss = ' + str(np.mean(loss_)))
-        print('Accuracy = ' + str(1 - np.mean(error_)))
+            deltaW = (outer_product(h0_probs, v0) 
+                      - outer_product(h1_probs, v1)).data.mean(0)
+            deltah = (h0_probs - h1_probs).data.mean(0)
+            deltav = (v0 - v1).data.mean(0)
+            
+            self.W.data     += lr * deltaW
+            self.hbias.data += lr * deltah
+            self.vbias.data += lr * deltav
+        return np.mean(error_)
 
         
-def test_rbm(hidd=200, learning_rates=[1e-3] * 10, k=2, batch_size=30, cuda_state=True):
+def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2, 
+             k_reconstruct=100, batch_size=30, model_dir='RBM.h5', 
+             best_dir='RBM_best.h5', use_gpu=True):
     
-    data = datasets.MNIST('mnist', train=True, download=True).train_data.type(torch.FloatTensor)
-    test = datasets.MNIST('mnist', train=False).test_data.type(torch.FloatTensor)
+    data = datasets.MNIST('mnist',
+                          train=True,
+                          download=True).train_data.type(torch.FloatTensor)
+    test = datasets.MNIST('mnist',
+                          train=False).test_data.type(torch.FloatTensor)
     
     data = data.view((-1, 784)) / 255
     data = data.bernoulli()                # Convert to binary values
@@ -138,54 +165,75 @@ def test_rbm(hidd=200, learning_rates=[1e-3] * 10, k=2, batch_size=30, cuda_stat
     test = test.bernoulli()
     
     vis  = len(data[0])
-    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True)
-
-    # According to Hinton this should be fine, but some biases diverge in the case of MNIST.
-    # Actually, this initialization is the inverse of the sigmoid. This is, it is the inverse of 
-    # p = sigm(vbias), so it can be expected that during training the weights are close to zero and
-    # change little
-    vbias = nn.Parameter(torch.log(data.mean(0) / (1 - data.mean(0))).clamp(-20, 20))
-	
-    # ------------------------------------------------------------------------------
-    # Construct RBM
-    # ------------------------------------------------------------------------------
-    pre_trained = os.path.isfile('RBM.h5')
-    rbm         = RBM(n_visible=vis, n_hidden=hidd, k=k, cuda_state=cuda_state, vbias=vbias)
-    if pre_trained:
-        rbm.load_state_dict(torch.load('RBM.h5'))
     
-    if cuda_state:
-        rbm=rbm.cuda()
-    # ------------------------------------------------------------------------------
+    # According to Hinton this initialization of the visible biases should be
+    # fine, but some biases diverge in the case of MNIST.
+    # Actually, this initialization is the inverse of the sigmoid. This is, it
+    # is the inverse of p = sigm(vbias), so it can be expected that during
+    # training the weights are close to zero and change little
+    vbias = nn.Parameter(torch.log(
+                                   data.mean(0) / (1 - data.mean(0))
+                                   ).clamp(-20, 20))
+	
+    # -------------------------------------------------------------------------
+    # Construct RBM
+    # -------------------------------------------------------------------------
+    pre_trained = os.path.isfile(model_dir)
+    rbm         = RBM(n_visible=vis,
+                      n_hidden=hidd,
+                      k=k,
+                      use_gpu=use_gpu,
+                      vbias=vbias)
+    if pre_trained:
+        rbm.load_state_dict(torch.load(best_dir))
+    
+    if use_gpu:
+        rbm = rbm.cuda()
+    # -------------------------------------------------------------------------
     # Training
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     if not pre_trained:
         validation = Variable(data)[:10000]
         test       = Variable(test)
-        if cuda_state:
-            validation = validation.cuda()
-            test       = test.cuda()
-        for epoch, learning_rate in enumerate(learning_rates):
-            train_op   = optim.SGD(rbm.parameters(), lr=learning_rate)
-            rbm.train(train_loader, train_op, epoch)
-	    # A good measure of well-fitting is the free energy difference between some known and
-	    # unknown instances. It is related to the log-likelihood difference, but it does not
-	    # depend on the partition function. It should be around 0, and if it grows, it might be
-	    # overfitting to the training data
-            gap = rbm.free_energy(validation) - rbm.free_energy(test)
-            print('Gap = ' + str(gap.data[0]))
-        torch.save(rbm.state_dict(), 'RBM.h5')
+        best_gap   = np.inf
+        look_ahead = 0
+        epoch = 1
+        while True:
+            train_loader = torch.utils.data.DataLoader(data,
+                                                       batch_size=batch_size,
+                                                       shuffle=True)
+            metrics      = rbm.train(train_loader, learning_rate, epoch)
+            # A good measure of well-fitting is the free energy difference
+            # between some known and unknown instances. It is related to the
+            # log-likelihood difference, but it does not depend on the
+            # partition function. It should be around 0, and if it grows, it
+            # might be overfitting to the training data.
+            # High-probability instances have very negative free energy, so the
+            # gap becoming very negative is sign of overfitting.
+            gap = (rbm.free_energy(validation) - rbm.free_energy(test)).mean(0)
+            print('Rec. error = ' + str(metrics))
+            print('Gap = {}'.format(gap.data[0]))
+            if abs(gap.data[0]) < best_gap:
+                best_gap = abs(gap.data[0])
+                print('Gap is smaller than best previous, saving weights...')
+                torch.save(rbm.state_dict(), best_dir)
+                look_ahead = 0
+            look_ahead += 1
+            if look_ahead == max_look_ahead:
+                print('It has been {} epochs since the last improvement. '
+                      .format(str(max_look_ahead)) + 'Stopping training...')
+                break
+            epoch += 1
+        torch.save(rbm.state_dict(), model_dir)
     
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Plotting
-    # ------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     print('Reconstructing images')
     plt.figure(figsize=(20, 10))
     zero = Variable(torch.zeros(25, 784))
-    if cuda_state:
-        zero = zero.cuda()
-    images = [zero.data.cpu().numpy().reshape((5 * 28, 5 * 28))]
-    for i in range(2000):
+    images = [zero.data.numpy().reshape((5 * 28, 5 * 28))]
+    for i in range(k_reconstruct):
         _, zero = rbm.sample_h_given_v(zero)
         _, zero = rbm.sample_v_given_h(zero)
         if i % 3 == 0:
@@ -195,9 +243,10 @@ def test_rbm(hidd=200, learning_rates=[1e-3] * 10, k=2, batch_size=30, cuda_stat
                 for l in range(5):
                     image[28*k:28*(k+1), 28*l:28*(l+1)] = datas[k + 5*l, :, :]
             images.append(image)
-    imageio.mimsave('rbm_sample.gif', images, duration=0.1)
+    imageio.mimsave('RBM_sample.gif', images, duration=0.1)
 
 
 if __name__ == "__main__":
-    learning_rates = [1e-2] * 10
-    test_rbm(hidd=30, learning_rates=learning_rates, k=2, batch_size=20, cuda_state=True)
+    test_rbm(hidd=30, learning_rate=1e-3, max_look_ahead=15, k=2,
+             k_reconstruct=2000, batch_size=10, model_dir='RBM.h5',
+             best_dir='RBM_best.h5', use_gpu=True)