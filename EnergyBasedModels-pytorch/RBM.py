842e1b5a5e1d0fc4ed90b5448ba294ea3899f00f
Alejandro Pozas-Kerstjens <apozas@users.noreply.github.com>
2018-02-22 12:57:31 +0100

    Small fixes
    CD code improved, added verbosity parameter (thanks @peterwittek), changed reconstruction error from binary_cross_entropy to mse_loss, use_gpu is set to False by default

--- EnergyBasedModels-pytorch/RBM.py
+++ EnergyBasedModels-pytorch/RBM.py
@@ -27,22 +27,22 @@ def outer_product(vecs1, vecs2):
        :type vecs1: list of torch.Tensor or torch.autograd.Variable
        :param vecs2: b 1-D tensors of length n
        :type vecs2: list of torch.Tensor or torch.autograd.Variable
-       
+
        :returns: torch.Tensor or torch.autograd.Variable of size (m, n)
        '''
     return torch.bmm(vecs1.unsqueeze(2), vecs2.unsqueeze(1))
-    
-    
+
+
 def log1pexp(tensor):
     '''Computes pointwise log(1+exp()) for all elements in a torch tensor. The
        way of computing it without under- or overflows is through the
        log-sum-exp trick, namely computing
-       
+
        log(1+exp(x)) = a + log(exp(-a) + exp(x-a))     with a = max(0, x)
-       
+
        The function is adapted to be used in GPU if needed.
        :param tensor: torch.Tensor or torch.autograd.Variable of arbitrary size
-       
+
        :returns: torch.Tensor or torch.autograd.Variable
        '''
     zr = Variable(torch.zeros(tensor.size()))
@@ -54,58 +54,55 @@ def log1pexp(tensor):
 
 class RBM(nn.Module):
     def __init__(self, n_visible=100, n_hidden=50, k=5,
-	             use_gpu=True, W=None, hbias=None, vbias=None):
-        
+	             use_gpu=False, verbose=0,
+                 W=None, hbias=None, vbias=None):
+
         super(RBM, self).__init__()
-        self.k        = k
-        self.use_gpu  = use_gpu
+        self.k = k
+        self.use_gpu = use_gpu
+        self.verbose = verbose
+
         if W is not None:
             self.W = W
         else:
             self.W = nn.Parameter(0.01 * torch.randn(n_hidden, n_visible))
-        
+
         if hbias is not None:
             self.hbias = hbias
         else:
             self.hbias = nn.Parameter(torch.zeros(n_hidden))
-        
+
         if vbias is not None:
             self.vbias = vbias
         else:
             self.vbias = nn.Parameter(torch.zeros(n_visible))
 
-
     def forward(self, v0):
-        h0_probs, h0 = self.sample_h_given_v(v0)
-        _, v         = self.sample_v_given_h(h0)
-        for _ in range(self.k - 1):
+        h0_probs, h = self.sample_h_given_v(v0)
+        for _ in range(self.k):
+            _, v = self.sample_v_given_h(h)
             h_probs, h = self.sample_h_given_v(v)
-            _ , v      = self.sample_v_given_h(h)
         return v0, h0_probs, v, h_probs
-        
-    
+
     def free_energy(self, v):
         if (self.use_gpu and not v.is_cuda):
             v = v.cuda()
-        vbias_term  = v.mv(self.vbias)
-        wx_b        = F.linear(v, self.W, self.hbias)
+        vbias_term = v.mv(self.vbias)
+        wx_b = F.linear(v, self.W, self.hbias)
         hidden_term = log1pexp(wx_b).sum(1)
         return (-hidden_term - vbias_term)
-        
-        
+
     def propdown(self, h):
         if (self.use_gpu and not h.is_cuda):
             h = h.cuda()
         pre_sigmoid_activation = F.linear(h, self.W.t(), self.vbias)
         return F.sigmoid(pre_sigmoid_activation)
 
-
     def propup(self, v):
         if (self.use_gpu and not v.is_cuda):
             v = v.cuda()
         pre_sigmoid_activation = F.linear(v, self.W, self.hbias)
         return F.sigmoid(pre_sigmoid_activation)
-
 
     def reconstruct(self, v, k=None):
         if k == None:
@@ -114,19 +111,16 @@ class RBM(nn.Module):
             _, h = self.sample_h_given_v(v)
             _, v = self.sample_v_given_h(h)
         return v
-        
-        
+
     def sample_h_given_v(self, v):
-        h_probs  = self.propup(v)
+        h_probs = self.propup(v)
         h_sample = h_probs.bernoulli()
         return [h_probs, h_sample]
 
-
     def sample_v_given_h(self, h):
-        v_probs  = self.propdown(h)
+        v_probs = self.propdown(h)
         v_sample = v_probs.bernoulli()
         return [v_probs, v_sample]
-
 
     def train(self, input_data, lr, epoch):
         error_ = []
@@ -136,22 +130,24 @@ class RBM(nn.Module):
             if self.use_gpu:
                 sample_data = sample_data.cuda()
             v0, h0_probs, v1, h1_probs = self.forward(sample_data)
-            rec_error = F.binary_cross_entropy(v1, v0)
-            error_.append(rec_error.data[0])
-            deltaW = (outer_product(h0_probs, v0) 
+            deltaW = (outer_product(h0_probs, v0)
                       - outer_product(h1_probs, v1)).data.mean(0)
             deltah = (h0_probs - h1_probs).data.mean(0)
             deltav = (v0 - v1).data.mean(0)
-            
-            self.W.data     += lr * deltaW
+
+            self.W.data += lr * deltaW
             self.hbias.data += lr * deltah
             self.vbias.data += lr * deltav
-        return np.mean(error_)
 
-        
-def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2, 
+            rec_error = F.mse_loss(v1, v0)
+            error_.append(rec_error.data[0])
+        if self.verbose > 0:
+            print('Reconstruction error = ' + str(np.mean(error_)))
+
+
+def test_rbm(hidd = 200, learning_rate = 1e-2, max_look_ahead = 100, k = 2,
              k_reconstruct=100, batch_size=30, model_dir='RBM.h5', 
-             best_dir='RBM_best.h5', use_gpu=True):
+             best_dir='RBM_best.h5', use_gpu=True, verbose=0):
     
     data = datasets.MNIST('mnist',
                           train=True,
@@ -183,7 +179,8 @@ def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2,
                       n_hidden=hidd,
                       k=k,
                       use_gpu=use_gpu,
-                      vbias=vbias)
+                      vbias=vbias,
+                      verbose=verbose)
     if pre_trained:
         rbm.load_state_dict(torch.load(best_dir))
     
@@ -202,7 +199,7 @@ def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2,
             train_loader = torch.utils.data.DataLoader(data,
                                                        batch_size=batch_size,
                                                        shuffle=True)
-            metrics      = rbm.train(train_loader, learning_rate, epoch)
+            rbm.train(train_loader, learning_rate, epoch)
             # A good measure of well-fitting is the free energy difference
             # between some known and unknown instances. It is related to the
             # log-likelihood difference, but it does not depend on the
@@ -211,7 +208,6 @@ def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2,
             # High-probability instances have very negative free energy, so the
             # gap becoming very negative is sign of overfitting.
             gap = (rbm.free_energy(validation) - rbm.free_energy(test)).mean(0)
-            print('Rec. error = ' + str(metrics))
             print('Gap = {}'.format(gap.data[0]))
             if abs(gap.data[0]) < best_gap:
                 best_gap = abs(gap.data[0])
@@ -249,4 +245,4 @@ def test_rbm(hidd=200, learning_rate=1e-2, max_look_ahead=100, k=2,
 if __name__ == "__main__":
     test_rbm(hidd=30, learning_rate=1e-3, max_look_ahead=15, k=2,
              k_reconstruct=2000, batch_size=10, model_dir='RBM.h5',
-             best_dir='RBM_best.h5', use_gpu=True)
+             best_dir='RBM_best.h5', use_gpu=False, verbose=1)
